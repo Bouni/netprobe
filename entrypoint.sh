46b1ea1bae7545a30279fdbdf8dc02b9024361d5
bouni <bouni@owee.de>
2025-03-10 13:07:49 +0100

    Adjust compose file and entrypoint to work with alpine based container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,11 @@
-if [[ "${MODULE}" == "NETPROBE" ]]; then python3 netprobe.py; elif [[ "${MODULE}" == "COLLECTOR" ]]; then python3 collector.py; elif [[ "${MODULE}" == "PRESENTATION" ]]; then python3 presentation.py; elif [[ "${MODULE}" == "SPEEDTEST" ]]; then python3 netprobe_speedtest.py; else /bin/bash; fi
+if [[ "${MODULE}" == "NETPROBE" ]]; then
+   uv run netprobe.py; 
+elif [[ "${MODULE}" == "COLLECTOR" ]]; then
+   uv run collector.py; 
+elif [[ "${MODULE}" == "PRESENTATION" ]]; then
+   uv run presentation.py; 
+elif [[ "${MODULE}" == "SPEEDTEST" ]]; then
+   uv run netprobe_speedtest.py; 
+else 
+  /bin/sh; 
+fi
