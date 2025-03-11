17cebb139dad44def0bc3d7ed3ff54349f668fe8
bouni <bouni@owee.de>
2025-03-11 15:51:35 +0100

    Adjust entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,3 @@
-if [[ "${MODULE}" == "NETPROBE" ]]; then
-   uv run netprobe.py; 
-elif [[ "${MODULE}" == "COLLECTOR" ]]; then
-   uv run collector.py; 
-elif [[ "${MODULE}" == "PRESENTATION" ]]; then
-   uv run presentation.py; 
-elif [[ "${MODULE}" == "SPEEDTEST" ]]; then
-   uv run netprobe_speedtest.py; 
-else 
-  /bin/sh; 
-fi
+#!/bin/sh
+
+uv run main.py; 
