1c2c3a9dc4c061aeebe8bb890a8d534deec4db58
bouni <bouni@owee.de>
2025-03-11 10:29:56 +0100

    Refactor config for yml file with pydantic validation

--- src/config.py
+++ src/config.py
@@ -1,64 +1,88 @@
-import os
-from dotenv import load_dotenv
+# Config loading and validation
 
-# Load configs from env
-
-try:  # Try to load env vars from file, if fails pass
-    load_dotenv()
-except Exception as e:
-    print(e)
-    pass
+import yaml
+from pydantic import BaseModel, ValidationError
 
 
-# Create class for each
+class PingModel(BaseModel):
+    sites: list[str]
 
 
-class Config_Netprobe:
-    probe_interval = int(os.getenv("PROBE_INTERVAL"))
-    probe_count = int(os.getenv("PROBE_COUNT"))
-    sites = os.getenv("SITES").split(",")
-    dns_test_site = os.getenv("DNS_TEST_SITE")
-    speedtest_enabled = os.getenv("SPEEDTEST_ENABLED", "False").lower() in (
-        "true",
-        "1",
-        "t",
-    )
-    speedtest_interval = int(os.getenv("SPEEDTEST_INTERVAL"))
-
-    DNS_NAMESERVER_1 = os.getenv("DNS_NAMESERVER_1")
-    DNS_NAMESERVER_1_IP = os.getenv("DNS_NAMESERVER_1_IP")
-    DNS_NAMESERVER_2 = os.getenv("DNS_NAMESERVER_2")
-    DNS_NAMESERVER_2_IP = os.getenv("DNS_NAMESERVER_2_IP")
-    DNS_NAMESERVER_3 = os.getenv("DNS_NAMESERVER_3")
-    DNS_NAMESERVER_3_IP = os.getenv("DNS_NAMESERVER_3_IP")
-    DNS_NAMESERVER_4 = os.getenv("DNS_NAMESERVER_4")
-    DNS_NAMESERVER_4_IP = os.getenv("DNS_NAMESERVER_4_IP")
-
-    nameservers = [
-        (DNS_NAMESERVER_1, DNS_NAMESERVER_1_IP),
-        (DNS_NAMESERVER_2, DNS_NAMESERVER_2_IP),
-        (DNS_NAMESERVER_3, DNS_NAMESERVER_3_IP),
-        (DNS_NAMESERVER_4, DNS_NAMESERVER_4_IP),
-    ]
+class NameServerModel(BaseModel):
+    name: str
+    ip: str
 
 
-class Config_Redis:
-    redis_url = os.getenv("REDIS_URL")
-    redis_port = os.getenv("REDIS_PORT")
-    redis_password = os.getenv("REDIS_PASSWORD")
+class DNSModel(BaseModel):
+    testsite: str
+    nameservers: list[NameServerModel]
 
 
-class Config_Presentation:
-    presentation_port = int(os.getenv("PRESENTATION_PORT"))
-    presentation_interface = os.getenv("PRESENTATION_INTERFACE")
-    device_id = os.getenv("DEVICE_ID")
+class HealthWeightModel(BaseModel):
+    loss: float
+    latency: float
+    jitter: float
+    dns_latency: float
 
-    weight_loss = float(os.getenv("weight_loss"))
-    weight_latency = float(os.getenv("weight_latency"))
-    weight_jitter = float(os.getenv("weight_jitter"))
-    weight_dns_latency = float(os.getenv("weight_dns_latency"))
 
-    threshold_loss = int(os.getenv("threshold_loss"))
-    threshold_latency = int(os.getenv("threshold_latency"))
-    threshold_jitter = int(os.getenv("threshold_jitter"))
-    threshold_dns_latency = int(os.getenv("threshold_dns_latency"))
+class HealthThresholdModel(BaseModel):
+    loss: int
+    latency: int
+    jitter: int
+    dns_latency: int
+
+
+class HealthModel(BaseModel):
+    weight: HealthWeightModel
+    threshold: HealthThresholdModel
+
+
+class SpeedModel(BaseModel):
+    enabled: bool
+    interval: int
+
+
+class PresentationModel(BaseModel):
+    port: int
+    interface: str
+
+
+class RedisModel(BaseModel):
+    url: str
+    port: int
+    password: str
+
+
+class ProbeModel(BaseModel):
+    interval: int
+    count: int
+
+
+class ConfigModel(BaseModel):
+    ping: PingModel
+    dns: DNSModel
+    health: HealthModel
+    speed: SpeedModel
+    presentation: PresentationModel
+    redis: RedisModel
+    probe: ProbeModel
+
+
+def load_config(file_path: str) -> dict:
+    try:
+        with open(file_path, "r") as file:
+            return yaml.safe_load(file)
+    except FileNotFoundError as e:
+        print(e)
+    return {}
+
+
+def validate_config(config_data: dict):
+    try:
+        config = ConfigModel(**config_data)
+        return config
+    except ValidationError as e:
+        print("Configuration validation failed:", e)
+
+
+config = validate_config(load_config("config.yml"))
